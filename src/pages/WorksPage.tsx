af6c2d84b897ed25236fbad9b662401a77eb3a04
Wailakkad <akkadouail8@gmail.com>
2026-05-26 11:56:59 +0200

    the bold orange link project is added with seccus

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -341,7 +341,7 @@ export default function WorksPage() {
                   </div>
                   {selectedProject.title !== 'Luma Finance Mobile App' && (
                     <a
-                      href={selectedProject.title === 'Natural Baby' ? 'https://rag-chatbot-pied-seven.vercel.app/' : selectedProject.title === 'OPTIC STUDIO' ? 'https://opticien-website-ai-agent-for-custo.vercel.app/' : selectedProject.title === 'Plumbing Services' ? 'https://plumber-jade.vercel.app/' : selectedProject.title === 'Next-Gen Agency' ? 'https://neuro-nexa.vercel.app/' : 'https://nextjs-ai-fullstack-store.vercel.app/'}
+                      href={selectedProject.title === 'Natural Baby' ? 'https://rag-chatbot-pied-seven.vercel.app/' : selectedProject.title === 'OPTIC STUDIO' ? 'https://opticien-website-ai-agent-for-custo.vercel.app/' : selectedProject.title === 'Plumbing Services' ? 'https://plumber-jade.vercel.app/' : selectedProject.title === 'Next-Gen Agency' ? 'https://neuro-nexa.vercel.app/' : selectedProject.title === 'Bold Orange' ? 'https://web-agency-landing-page-flame.vercel.app/' : 'https://nextjs-ai-fullstack-store.vercel.app/'}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="hidden md:flex items-center gap-2 bg-green text-black px-6 py-3 font-bold text-xs uppercase tracking-wider hover:scale-105 transition-transform interactive"
